79262bad9cf42deafca6adc1b961f508bb38c615
Kirill-Ryzhenko <kirillryzh@gmail.com>
2021-03-01 20:46:30 +0300

    fix rate star

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ $(function () {
     rating: 5,
     starWidth: '11px',
     readOnly: true,
+    ratedFill: '#ffa726',
+    spacing: '2px',
   });
   var mixer = mixitup('.product__inner-box');
 });
